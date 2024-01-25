e7d746fbf4d75771f03b91c15114006bc7e255ea
kuroko84 <ttphong071016@gmail.com>
2024-01-26 00:18:11 +0700

    73- create folder tictactoe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from "./Assets/logo.svg";
+// import logo from "./Assets/logo.svg";
 import "./App.css";
-import Header from "./Components/Header/Header";
-import Example from "./Components/Example/Example.jsx";
+// import Header from "./Components/Header/Header";
+// import Example from "./Components/Example/Example.jsx";
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <Header logo={logo} />
+  //     <Example />
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <Header logo={logo} />
-      <Example />
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
   );
 }
 
