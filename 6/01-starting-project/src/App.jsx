6947f12bb24f201a200dc637d7fd578ce2457d3d
kuroko84 <ttphong071016@gmail.com>
2024-02-02 23:43:30 +0700

    dymanic & conditional styling with styled components

--- 6/01-starting-project/src/App.jsx
+++ 6/01-starting-project/src/App.jsx
@@ -1,64 +1,13 @@
-import Header from "./component/Header/Header";
-import UserInput from "./component/UserInput/UserInput";
-import Result from "./component/Result/Result";
-import Message from "./component/Message/Message";
-import React from "react";
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-function App() {
-  const [userInput, setUserInput] = React.useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 12,
-  });
-  const [isPopupVisible, setPopupVisible] = React.useState(false);
-  const [flag, setFlag] = React.useState({
-    initialInvestment: true,
-    annualInvestment: true,
-    expectedReturn: true,
-    duration: true,
-  });
-
-  const openPopup = () => {
-    setPopupVisible(true);
-  };
-
-  const closePopup = () => {
-    setPopupVisible(false);
-  };
-
-  let valid = true;
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((preUserInput) => {
-      return {
-        ...preUserInput,
-        [inputIdentifier]: +newValue,
-      };
-    });
-    setFlag({
-      initialInvestment: userInput.initialInvestment > 0,
-      annualInvestment: userInput.annualInvestment > 0,
-      expectedReturn: userInput.expectedReturn > 0,
-      duration: userInput.duration > 0,
-    });
-
-    valid =
-      flag.annualInvestment &&
-      flag.initialInvestment &&
-      flag.expectedReturn &&
-      flag.duration;
-  }
-  console.log(valid);
-
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput onChange={handleChange} userInput={userInput} />
-      {!valid && <Message notification={"Please enter valid number"} />}
-      {valid && <Result input={userInput} />}
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
